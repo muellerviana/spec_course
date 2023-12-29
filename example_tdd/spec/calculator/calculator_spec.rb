2c26e9e384ff335fe8e72f2df0a48c72701116ec
muellerviana <muellerviana@gmail.com>
2023-12-29 15:22:03 -0300

    aula 12 concluída

--- example_tdd/spec/calculator/calculator_spec.rb
+++ example_tdd/spec/calculator/calculator_spec.rb
@@ -1,15 +1,25 @@
 require './lib/Calculator.rb'
 
-describe Calculator do 
-  it 'sum method for 2 numbers' do
-    calc = Calculator.new
-    result = calc.sum(6, 4)
-    expect(result).to eq(10)
-  end
+describe Calculator do
 
-  it 'sum method for 2 negative numbers' do
-    calc = Calculator.new
-    result = calc.sum(-2, 4)
-    expect(result).to eq(2)
+  subject(:calc) { described_class.new()  } 
+
+  context '#sum' do
+    it 'positive numbers' do
+      result = calc.sum(6, 4)
+      expect(result).to eq(10)
+    end
+
+    it 'negative with positive numbers' do
+      result = calc.sum(-2, 4)
+      expect(result).to eq(2)
+    end
+
+    it 'negative numbers' do
+      result = calc.sum(-2, -4)
+      expect(result).to eq(-6)
+    end
+
+    it 'with numbers'
   end
 end
